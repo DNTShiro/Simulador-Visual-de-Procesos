2455bd35158dbca32c4ec4d9d498a114a44c738d
leoCastro23 <0512025084@alumnos.unp.edu.pe>
2026-07-15 16:25:30 -0500

    Create ResultadoSimulacion.java

---
+++ ResultadoSimulacion.java
@@ -0,0 +1,17 @@
+import modelo.Proceso;
+import java.utilList;
+
+public call ResultadoSimulacion{
+  public List<BlloqueGantt> LineaTiempo;
+  public List<Proceso> procesos;
+  public List<String> mensajes;
+  public int tiempoTotal;
+
+  public ResultadoSimuladocion(List<BloqueGantt> lineaTiempo, List<Proceso> procesos;List<String> mensajes, int tiempoTotal){
+    this.lineaTiempo = lineaTiempo ;
+    this.procesos = procesos ;
+    this.mensajes = mensajes;
+    this.tiempoTotal = tiempoTotal;
+  }
+}
+    
