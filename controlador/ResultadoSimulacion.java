b8223dd196d697f46b01360790c939206db355ed
0512025085-tech <0512025085@alumnos.unp.edu.pe>
2026-07-17 13:34:09 -0500

    Update ResultadoSimulacion.java

--- controlador/ResultadoSimulacion.java
+++ controlador/ResultadoSimulacion.java
@@ -1,23 +1,40 @@
 package controlador;
 
-/**
- * Representa un tramo de tiempo en el que un proceso ocupo la CPU.
- * Se usa tanto para dibujar el Diagrama de Gantt como para animar la simulacion.
- */
-public class BloqueGantt {
-    public final int pid;
-    public final String nombre;
-    public final int inicio; // tiempo en que comienza el bloque
-    public final int fin;    // tiempo en que termina el bloque (exclusivo)
+import modelo.Proceso;
+import java.util.List;
 
-    public BloqueGantt(int pid, String nombre, int inicio, int fin) {
-        this.pid = pid;
-        this.nombre = nombre;
-        this.inicio = inicio;
-        this.fin = fin;
+/**
+ * Guarda todo lo que produce el planificador: la linea de tiempo (para
+ * el diagrama de Gantt), los procesos con sus tiempos ya calculados y
+ * los mensajes de advertencia (por ejemplo, de memoria insuficiente).
+ */
+public class ResultadoSimulacion {
+    public List<BloqueGantt> lineaTiempo;
+    public List<Proceso> procesos;
+    public List<String> mensajes;
+    public int tiempoTotal;
+
+    public ResultadoSimulacion(List<BloqueGantt> lineaTiempo, List<Proceso> procesos,
+                                List<String> mensajes, int tiempoTotal) {
+        this.lineaTiempo = lineaTiempo;
+        this.procesos = procesos;
+        this.mensajes = mensajes;
+        this.tiempoTotal = tiempoTotal;
     }
 
-    public int duracion() {
-        return fin - inicio;
+    public double getEsperaPromedio() {
+        int suma = 0;
+        for (int i = 0; i < procesos.size(); i++) {
+            suma = suma + procesos.get(i).getTiempoEspera();
+        }
+        return (double) suma / procesos.size();
+    }
+
+    public double getRetornoPromedio() {
+        int suma = 0;
+        for (int i = 0; i < procesos.size(); i++) {
+            suma = suma + procesos.get(i).getTiempoRetorno();
+        }
+        return (double) suma / procesos.size();
     }
 }
